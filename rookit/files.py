b5be192127462e047f0f602303f960344f9d595a
Andreas Bresser <a.bresser@greyrook.com>
2015-11-05 21:32:11 +0100

    copy using task_for_file

--- rookit/files.py
+++ rookit/files.py
@@ -49,24 +49,36 @@ def create_dist_path(out_file):
         os.makedirs(out_dir)
 
 
-def copy_file_task(src_path, dist_path, filename, task_dep=None):
-    """
-    generate task to copy single file "filename" from src_path to dist_path
-    """
+def task_for_file(task, src_path, dist_path, filename, task_dep=None):
     out_file = abs_path(dist_path, filename)
     in_file = abs_path(src_path, filename)
 
     create_dist_path(out_file)
 
-    task = {
+    _task = {
         'name': '{} --> {}'.format(in_file, out_file),
-        'actions': [(shutil.copy, [in_file, out_file])],
+        'actions': [(task, [in_file, out_file])],
         'targets': [out_file],
         'file_dep': [in_file],
     }
     if task_dep:
-        task['task_dep'] = task_dep
-    return task
+        _task['task_dep'] = task_dep
+    yield _task
+
+
+def task_for_files(task, src_path, dist_path, files, task_dep=None):
+    """
+    execute a task for all files
+    """
+    for filename in files:
+        yield task_for_file(task, src_path, dist_path, filename, task_dep=task_dep)
+
+
+def copy_file_task(src_path, dist_path, filename, task_dep=None):
+    """
+    generate task to copy single file "filename" from src_path to dist_path
+    """
+    yield task_for_file(shutil.copy, src_path, dist_path, filename, task_dep=task_dep)
 
 
 def copy_files_task(src_path, dist_path, ext=None,
@@ -81,25 +93,3 @@ def copy_files_task(src_path, dist_path, ext=None,
         # TODO: mengle with copy_file_task
         task = copy_file_task(src_path, dist_path, filename, task_dep=task_dep)
         yield task
-
-
-
-def task_for_files(task, src_path, dist_path, files, task_dep=None):
-    """
-    execute a task for all files
-    """
-    for filename in files:
-        out_file = abs_path(dist_path, filename)
-        in_file = abs_path(src_path, filename)
-
-        create_dist_path(out_file)
-
-        _task = {
-            'name': in_file + ' --> ' + out_file,
-            'actions': [(task, [in_file, out_file])],
-            'targets': [out_file],
-            'file_dep': [in_file],
-        }
-        if task_dep:
-            _task['task_dep'] = task_dep
-        yield _task
