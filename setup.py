7482c848d29948dc6872b3b9e09806e33957898b
Fabian Elsner <f.elsner@greyrook.com>
2020-08-10 16:21:57 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='rookit',
-      version='0.1.0',
+      version='0.1.1',
       description='grey rook scripts for doit',
       author='Andreas Bresser',
       author_email='a.bresser@greyrook.com',
